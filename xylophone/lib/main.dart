d4f655ebaa0bdf2fcda20cd5ace334031ca9bc93
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-02-25 22:38:59 +0545

    shortening the working code and some design

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -4,106 +4,104 @@ import 'package:audioplayers/audioplayers.dart';
 void main() {
   runApp(const XylophoneApp());
 }
+
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
+  void playSound(int soundNumber) {
+    //final player = AudioCache(); //no longer use of audiocacache
+    final player = AudioPlayer();
+    player.play(
+      AssetSource('assets_note$soundNumber.wav'),
+    );
+    //player.play('note1.wav');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: TextButton(
-              onPressed: () async{
-              //final player = AudioCache(); //no longer use of audiocacache
-              final player = AudioPlayer();
-              await player.play(AssetSource('assets_note1.wav'),
-              );
-              //player.play('note1.wav');
-        },
-        child: const Text('Click me1'),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              
+              child: TextButton(
+                onPressed: () {
+                  playSound(1);
+                },
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.red),
+                ),
+                child: const Text(' '),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  playSound(2);
+                },
+                // child: const Text('Click me2'),
+                //  child: Container(
+                //  //padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                //   color: Colors.orange,
+                //   child: const Text(" "),
+                //   ),
+                style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                child: const Text(' '),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  playSound(3);
+                },
+                style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                child: const Text(' '),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  playSound(4);
+                },
+                style: TextButton.styleFrom(backgroundColor: Colors.green),
+                child: const Text(' '),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  playSound(5);
+                },
+                style: TextButton.styleFrom(backgroundColor: Colors.teal),
+                child: const Text(' '),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  playSound(6);
+                },
+                style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                child: const Text(' '),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  playSound(7);
+                },
+                style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                child: const Text(' '),
+              ),
+            ),
+          ],
         ),
-        ),
-        Expanded(child: TextButton(
-              onPressed: () async{
-              //final player = AudioCache(); //no longer use of audiocacache
-              final player = AudioPlayer();
-              await player.play(AssetSource('assets_note2.wav'),
-              );
-              //player.play('note1.wav');
-        },
-        child: const Text('Click me2'),
-        ),),
-        Expanded(
-                child: TextButton(
-              onPressed: () async{
-              //final player = AudioCache(); //no longer use of audiocacache
-              final player = AudioPlayer();
-              await player.play(AssetSource('assets_note3.wav'),
-              );
-              //player.play('note1.wav');
-        },
-        
-        child: const Text('Click me3'),
-        ),
-        ),
-        Expanded(
-                child: TextButton(
-              onPressed: () async{
-              //final player = AudioCache(); //no longer use of audiocacache
-              final player = AudioPlayer();
-              await player.play(AssetSource('assets_note4.wav'),
-              );
-              //player.play('note1.wav');
-        },
-        child: const Text('Click me4'),
-        ),
-        ),
-        Expanded(
-                child: TextButton(
-              onPressed: () async{
-              //final player = AudioCache(); //no longer use of audiocacache
-              final player = AudioPlayer();
-              await player.play(AssetSource('assets_note5.wav'),
-              );
-              //player.play('note1.wav');
-        },
-        child: const Text('Click me5'),
-        ),
-        ),
-        Expanded(
-                child: TextButton(
-              onPressed: () async{
-              //final player = AudioCache(); //no longer use of audiocacache
-              final player = AudioPlayer();
-              await player.play(AssetSource('assets_note6.wav'),
-              );
-              //player.play('note1.wav');
-        },
-        child: const Text('Click me6'),
-        ),
-        ),
-        Expanded(
-                child: TextButton(
-              onPressed: () async{
-              //final player = AudioCache(); //no longer use of audiocacache
-              final player = AudioPlayer();
-              await player.play(AssetSource('assets_note7.wav'),
-              );
-              //player.play('note1.wav');
-        },
-        child: const Text('Click me7'),
-        ),
-        ),
-            ],
-          )
         ),
       ),
-
     );
-}
-
+  }
 }
